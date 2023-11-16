fdd386f685f2e26362611f919baf4069dd022456
haki-user <adityapratapsingh33@outlook.com>
2023-11-16 23:39:52 +0530

    nearly pixel perfect navbar

--- apps/user/components/Navbar.tsx
+++ apps/user/components/Navbar.tsx
@@ -5,21 +5,22 @@ type NavbarProps = {};
 
 const Navbar: React.FC<NavbarProps> = () => {
     return (
-        <div className="navbar flex items-center justify-between sm:px-12 px-2 md:px-24">
-            <Link href="/" className="flex items-center justify-center h-20">
-                <div className="w-40 h-5 flex items-center">
+        <div className="navbar flex items-center justify-between min-[1200px]:w-[1170px] h-10 px-[15px]  mx-auto mt-[20px]">
+            <Link href="/" className="flex items-center justify-center">
+                <div className="flex items-center">
                     <img src="/logo.png" alt="Leetcode" className="h-10" />
-                    <span className="text-white text-2xl font-bold ml-2">NeatCode</span>
+                    <span className="text-white text-2xl font-TypoRound font-bold pl-[10px] h-10 p-[2px]" 
+                    style={{ fontWeight: 100 }}>NeatCode</span>
                 </div>
             </Link>
-            <div className="nav-right">
-                <div className="flex items-center">
+            <div className="nav-right h-10">
+              <div className="nav-menu h-full mb-auto mt-1 pr-2">
                     <NavbarItem href="/premium" text="Premium" isOrange={true} />
                     <NavbarItem href="/explore" text="Explore" />
                     <NavbarItem href="/product" text="Product" />
                     <NavbarItem href="/developer" text="Developer" />
-                    <NavbarItem href="/signin" text="Sign in" />
-                </div>
+                    <NavbarItem href="/signin" text="Sign in" isLast={true} />
+              </div>
             </div>
         </div>
     );
@@ -29,16 +30,13 @@ type NavbarItemProps = {
     href: string;
     text: string;
     isOrange?: boolean;
+    isLast?: boolean;
 };
-const NavbarItem: React.FC<NavbarItemProps> = ({ text, href, isOrange }) => {
+const NavbarItem: React.FC<NavbarItemProps> = ({ text, href, isOrange, isLast }) => {
     return (
-    <div className="mr-7">
-        <Link href={href}  
-            className={`${isOrange?"text-brand-orange":"text-white"} px-2 py-1 sm:px-4 rounded-full text-sm font-medium border-1 border-solid border-transparent hover:text-black ${isOrange?"hover:bg-brand-orange":"hover:bg-white"} hover:border-1 hover:rounded-full ${isOrange?"hover:border-brand-orange":
-            "hover:border-white"} transition duration-300 ease-in-out`} >
-            { text }
-        </Link>
-    </div>
+        <a href={href} className={isLast?"mr-0":"mr-[25px]"} style={{ textDecoration: "none", outline: "none", letterSpacing: "0em", }}>
+            <span className={`tracking-normal px-[15px] pt-2 pb-[10px] rounded-full text-[15px] font-NimbusSans border-1 border-solid border-transparent ${isOrange?"text-brand-orange":"text-white"} hover:text-black ${isOrange?"hover:bg-brand-orange":"hover:bg-white"} transition-all duration-[400ms] ease-in-out`}>{ text }</span>
+        </a>
     );
 };
 export default Navbar;
